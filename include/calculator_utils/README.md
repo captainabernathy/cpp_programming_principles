a41882352ab03a42b26ce552cc73ed8e627ee26c
Aaron Lafont <captainabernathy@gmail.com>
2023-06-10 01:14:39 -0500

    calculator library interfaces and readme

--- include/calculator_utils/README.md
+++ include/calculator_utils/README.md
@@ -1,6 +1,7 @@
 ## Calculator Library - Interfaces to libcalculator.a
 
 ### Interfaces
+* **calculator_utils.hpp**
 * **calculator.hpp**
 * **calc_constants.hpp**
 * **token.hpp**
--- include/calculator_utils/calculator_utils.hpp
+++ include/calculator_utils/calculator_utils.hpp
@@ -1,7 +1,32 @@
 #ifndef CALCULATOR_UTILS_HPP
 #define CALCULATOR_UTILS_HPP
 
-// calculator.hpp includes all of the headers needed to build a Calculator
+// provides:
+// Calculator (class)
+//      Calculator()
+//      double get_value(string var)
+//      bool is_declared(string var)
+//      double define_name(string var, double val)
+//      void set_value(string var, double val)
+//
+// Token (class)
+//      char kind
+//      double value
+//      string name
+//      Token(char ch)
+//      Token(char ch, double val)
+//      Token(char ch, string str)
+//
+// Token_stream (class)
+//      Token_stream()
+//      Token get()
+//      void putback(Token t)
+//      void ignore(char ch)
+//
+// Variable
+//      string name
+//      double value
+//      Variable(string n, double v)
 #include <calculator_utils/calculator.hpp>
 
 #endif
